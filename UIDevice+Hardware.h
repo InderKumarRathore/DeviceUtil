5ea306a88dc8a2f2ce03e9c363d0f202bde8b7ff
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2014-10-24 14:20:14 +0530

    Added iPhone 6 and 6 plus hardware string (based on request)

--- UIDevice+Hardware.h
+++ UIDevice+Hardware.h
@@ -20,50 +20,53 @@
 
 typedef enum
 {
-    NOT_AVAILABLE,
-    
-    IPHONE_2G,
-    IPHONE_3G,
-    IPHONE_3GS,
-    IPHONE_4,
-    IPHONE_4_CDMA,
-    IPHONE_4S,
-    IPHONE_5,
-    IPHONE_5_CDMA_GSM,
-    IPHONE_5C,
-    IPHONE_5C_CDMA_GSM,
-    IPHONE_5S,
-    IPHONE_5S_CDMA_GSM,
-    
-    IPOD_TOUCH_1G,
-    IPOD_TOUCH_2G,
-    IPOD_TOUCH_3G,
-    IPOD_TOUCH_4G,
-    IPOD_TOUCH_5G,
-    
-    IPAD,
-    IPAD_2,
-    IPAD_2_WIFI,
-    IPAD_2_CDMA,
-    IPAD_3,
-    IPAD_3G,
-    IPAD_3_WIFI,
-    IPAD_3_WIFI_CDMA,
-    IPAD_4,
-    IPAD_4_WIFI,
-    IPAD_4_GSM_CDMA,
-    
-    IPAD_MINI,
-    IPAD_MINI_WIFI,
-    IPAD_MINI_WIFI_CDMA,
-    IPAD_MINI_RETINA_WIFI,
-    IPAD_MINI_RETINA_WIFI_CDMA,
-    
-    IPAD_AIR_WIFI,
-    IPAD_AIR_WIFI_GSM,
-    IPAD_AIR_WIFI_CDMA,
-    
-    SIMULATOR
+  NOT_AVAILABLE,
+  
+  IPHONE_2G,
+  IPHONE_3G,
+  IPHONE_3GS,
+  IPHONE_4,
+  IPHONE_4_CDMA,
+  IPHONE_4S,
+  IPHONE_5,
+  IPHONE_5_CDMA_GSM,
+  IPHONE_5C,
+  IPHONE_5C_CDMA_GSM,
+  IPHONE_5S,
+  IPHONE_5S_CDMA_GSM,
+
+  IPHONE_6_PLUS,
+  IPHONE_6,
+
+  IPOD_TOUCH_1G,
+  IPOD_TOUCH_2G,
+  IPOD_TOUCH_3G,
+  IPOD_TOUCH_4G,
+  IPOD_TOUCH_5G,
+
+  IPAD,
+  IPAD_2,
+  IPAD_2_WIFI,
+  IPAD_2_CDMA,
+  IPAD_3,
+  IPAD_3G,
+  IPAD_3_WIFI,
+  IPAD_3_WIFI_CDMA,
+  IPAD_4,
+  IPAD_4_WIFI,
+  IPAD_4_GSM_CDMA,
+
+  IPAD_MINI,
+  IPAD_MINI_WIFI,
+  IPAD_MINI_WIFI_CDMA,
+  IPAD_MINI_RETINA_WIFI,
+  IPAD_MINI_RETINA_WIFI_CDMA,
+
+  IPAD_AIR_WIFI,
+  IPAD_AIR_WIFI_GSM,
+  IPAD_AIR_WIFI_CDMA,
+
+  SIMULATOR
 } Hardware;
 
 
@@ -83,11 +86,8 @@ typedef enum
 /** This method returns YES if the current device is better than the hardware passed */
 - (BOOL)isCurrentDeviceHardwareBetterThan:(Hardware)hardware;
 
-/** This method returns the resolution for still image that can be received 
+/** This method returns the resolution for still image that can be received
  from back camera of the current device. Resolution returned for image oriented landscape right. **/
 - (CGSize)backCameraStillImageResolutionInPixels;
-
-/** This method returns YES if the currend device is iPhone and has 4" display **/
-- (BOOL)isIphoneWith4inchDisplay;
 
 @end
